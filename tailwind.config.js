b89d42b8c8ddd1b183637058b7af0996f2939f73
williamrolando88 <william.rolando88@hotmail.com>
2021-11-15 15:42:05 -0500

    Install TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js', '**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
