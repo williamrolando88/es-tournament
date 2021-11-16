8586003c99ba474819c21972d3f00cb7d0881c59
williamrolando88 <william.rolando88@hotmail.com>
2021-11-16 18:03:51 -0500

    Add finish styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,23 @@ module.exports = {
         lato: ['Lato', 'sans-serif'],
       },
       backgroundImage: {
-        main: "url('../../assets/background/gaming-pattern.png')",
-        program: "url('../../assets/background/5-dots.png')",
+        main: "url('../../assets/img/bg-main.png')",
+        program: "url('../../assets/img/bg-program.png')",
+        feature: "url('../../assets/img/bg-feature.jpg')",
+        partners: "url('../../assets/img/bg-partners.png')",
+        tournament1: "url('../../assets/img/bg-tournament1.jpg')",
+        tournament2: "url('../../assets/img/bg-tournament2.jpeg')",
+        tournament3: "url('../../assets/img/bg-tournament3.jpg')",
+      },
+      colors: {
+        primary: {
+          black: '#272a31',
+          orange: '#ec5242',
+          gray: '#d3d3d3',
+        },
+      },
+      backgroundSize: {
+        '50c': '50% 50%',
       },
     },
   },
