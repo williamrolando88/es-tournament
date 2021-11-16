fe85639568be58d98a09b8800a6d35d42ad3bd72
williamrolando88 <william.rolando88@hotmail.com>
2021-11-15 19:43:10 -0500

    Add style to main section - mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js', '**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cocogoose: ['COCOGOOSE', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+      },
+      backgroundImage: {
+        main: "url('../../assets/background/gaming-pattern.png')",
+        program: "url('../../assets/background/5-dots.png')",
+      },
+    },
   },
   variants: {
     extend: {},
